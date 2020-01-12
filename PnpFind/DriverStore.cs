7d18d67b3bd9d099432c5e797ad0047777a8b94a
Benjamin Höglinger-Stelzer <benjamin.hoeglinger@nefarius.at>
2020-01-12 23:09:58 +0100

    Re-sharped code

--- PnpFind/DriverStore.cs
+++ PnpFind/DriverStore.cs
@@ -9,72 +9,70 @@ namespace PnpFind
 {
     public class DriverStore
     {
-        private const Int32 INF_STYLE_OLDNT = 0x00000001;
-        private const Int32 INF_STYLE_WIN4 = 0x00000002;
-        private const Int64 INVALID_HANDLE_VALUE = -1;
+        private const int INF_STYLE_OLDNT = 0x00000001;
+        private const int INF_STYLE_WIN4 = 0x00000002;
+        private const long INVALID_HANDLE_VALUE = -1;
 
-        public Dictionary<string, string> OemInfEntities=new Dictionary<string, string>();
-
-
-            [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
-        public struct INFCONTEXT
-        {
-            private IntPtr Inf;
-            private IntPtr CurrentInf;
-            private uint Section;
-            private uint Line;
-        }
+        public Dictionary<string, string> OemInfEntities = new Dictionary<string, string>();
 
         [DllImport("setupapi.dll", CharSet = CharSet.Auto, SetLastError = true)]
         public static extern IntPtr SetupOpenInfFile([MarshalAs(UnmanagedType.LPTStr)] string FileName,
-                                                     [MarshalAs(UnmanagedType.LPTStr)] string InfClass, Int32 InfStyle, out uint ErrorLine);
+            [MarshalAs(UnmanagedType.LPTStr)] string InfClass, int InfStyle, out uint ErrorLine);
 
         [DllImport("setupapi.dll", CharSet = CharSet.Auto, SetLastError = true)]
         public static extern bool SetupFindFirstLine(IntPtr InfHandle, [MarshalAs(UnmanagedType.LPTStr)] string Section,
-                                                     [MarshalAs(UnmanagedType.LPTStr)] string Key, ref INFCONTEXT Context);
+            [MarshalAs(UnmanagedType.LPTStr)] string Key, ref INFCONTEXT Context);
 
         [DllImport("setupapi.dll", CharSet = CharSet.Auto, SetLastError = true)]
         public static extern bool SetupFindNextLine(ref INFCONTEXT ContextIn, out INFCONTEXT ContextOut);
 
         [DllImport("setupapi.dll", CharSet = CharSet.Auto, SetLastError = true)]
-        public static extern bool SetupGetStringField(ref INFCONTEXT Context, Int32 FieldIndex, [MarshalAs(UnmanagedType.LPTStr)] string ReturnBuffer,
-                                                      Int32 ReturnBufferSize, out Int32 RequiredSize);
+        public static extern bool SetupGetStringField(ref INFCONTEXT Context, int FieldIndex,
+            [MarshalAs(UnmanagedType.LPTStr)] string ReturnBuffer,
+            int ReturnBufferSize, out int RequiredSize);
 
         [DllImport("setupapi.dll", CharSet = CharSet.Auto, SetLastError = true)]
-        public static extern bool SetupGetStringField(ref INFCONTEXT Context, Int32 FieldIndex, [MarshalAs(UnmanagedType.LPTStr)] StringBuilder ReturnBuffer,
-                                                      Int32 ReturnBufferSize, out Int32 RequiredSize);
-        
+        public static extern bool SetupGetStringField(ref INFCONTEXT Context, int FieldIndex,
+            [MarshalAs(UnmanagedType.LPTStr)] StringBuilder ReturnBuffer,
+            int ReturnBufferSize, out int RequiredSize);
+
         [DllImport("setupapi.dll", CharSet = CharSet.Auto, SetLastError = true)]
         public static extern void SetupCloseInfFile(IntPtr InfHandle);
 
         public static int RemoveOemInf(string oemInfFileName)
         {
-            Process processPnpUtil = new Process();
-
-
-            processPnpUtil.StartInfo.UseShellExecute = false;
-            processPnpUtil.StartInfo.FileName = "pnputil.exe";
-            processPnpUtil.StartInfo.Arguments = "-f -d " + oemInfFileName;
-            processPnpUtil.StartInfo.CreateNoWindow = true;
+            var processPnpUtil = new Process
+            {
+                StartInfo =
+                {
+                    UseShellExecute = false,
+                    FileName = "pnputil.exe",
+                    Arguments = "-f -d " + oemInfFileName,
+                    CreateNoWindow = true
+                }
+            };
 
             if (processPnpUtil.Start())
             {
                 processPnpUtil.WaitForExit();
                 return processPnpUtil.ExitCode;
             }
+
             return -1;
         }
 
         public static DirectoryInfo GetWindowsDirectory()
         {
-            string sys32 = Environment.GetFolderPath(Environment.SpecialFolder.System);
-            DirectoryInfo diWindows = new DirectoryInfo(sys32);
+            var sys32 = Environment.GetFolderPath(Environment.SpecialFolder.System);
+            var diWindows = new DirectoryInfo(sys32);
             return diWindows.Parent;
         }
+
         public static DirectoryInfo GetWindowsInfDirectory()
         {
             return new DirectoryInfo(Path.Combine(GetWindowsDirectory().FullName, "inf"));
         }
+
         public static FileInfo GetOemInfFullPath(string name)
         {
             if (!name.ToLower().EndsWith(".inf"))
@@ -88,33 +86,30 @@ namespace PnpFind
             return new List<FileInfo>(GetWindowsInfDirectory().GetFiles("oem*.inf"));
         }
 
-        public static bool GetInfSection(string infFile, string section, out Dictionary<string, List<string>> OemInfEntities)
+        public static bool GetInfSection(string infFile, string section,
+            out Dictionary<string, List<string>> OemInfEntities)
         {
             uint ErrorLine = 0;
-            OemInfEntities=new Dictionary<string, List<string>>();
-            IntPtr infHandle = SetupOpenInfFile(infFile, null, INF_STYLE_OLDNT | INF_STYLE_WIN4, out ErrorLine);
-            int iCode = Marshal.GetLastWin32Error();
+            OemInfEntities = new Dictionary<string, List<string>>();
+            var infHandle = SetupOpenInfFile(infFile, null, INF_STYLE_OLDNT | INF_STYLE_WIN4, out ErrorLine);
+            var iCode = Marshal.GetLastWin32Error();
             if (infHandle.ToInt64() != INVALID_HANDLE_VALUE)
             {
-                INFCONTEXT Context = new INFCONTEXT();
-                if (SetupFindFirstLine(infHandle, section, null, ref Context) == true)
-                {
-                    
+                var Context = new INFCONTEXT();
+                if (SetupFindFirstLine(infHandle, section, null, ref Context))
                     do
                     {
-                        StringBuilder sb = new StringBuilder(1024);
-                        string valueName = String.Empty;
-                        List<string> valueText=new List<string>();
-                        int fieldIndex = 0;
+                        var sb = new StringBuilder(1024);
+                        var valueName = string.Empty;
+                        var valueText = new List<string>();
+                        var fieldIndex = 0;
                         int requiredSize;
 
                         while (SetupGetStringField(ref Context, fieldIndex++, sb, sb.Capacity, out requiredSize))
-                        {
-                            if (fieldIndex==1)
-                                valueName = sb.ToString(0, requiredSize-1);
+                            if (fieldIndex == 1)
+                                valueName = sb.ToString(0, requiredSize - 1);
                             else
-                                valueText.Add(sb.ToString(0, requiredSize-1));
-                        }
+                                valueText.Add(sb.ToString(0, requiredSize - 1));
                         try
                         {
                             OemInfEntities.Add(valueName, valueText);
@@ -123,25 +118,29 @@ namespace PnpFind
                         {
                             Console.WriteLine("Skipping duplicate {0}", valueName);
                         }
-
-
                     } while (SetupFindNextLine(ref Context, out Context));
-                }
                 else
-                {
-                    Console.WriteLine("Can't find {0} section.",section);
-                }
+                    Console.WriteLine("Can't find {0} section.", section);
+
                 SetupCloseInfFile(infHandle);
             }
             else
             {
                 Console.WriteLine("Failed to open INF file. Error code - {0}.", iCode);
-                if (ErrorLine != 0)
-                {
-                    Console.WriteLine("Failure line - {0}.", ErrorLine);
-                }
+                if (ErrorLine != 0) Console.WriteLine("Failure line - {0}.", ErrorLine);
             }
+
             return false;
+        }
+
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+        public struct INFCONTEXT
+        {
+            private readonly IntPtr Inf;
+            private readonly IntPtr CurrentInf;
+            private readonly uint Section;
+            private readonly uint Line;
         }
     }
 }
--- PnpFind/Program.cs
+++ PnpFind/Program.cs
@@ -1,21 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
 
 namespace PnpFind
 {
     internal class Program
     {
-        private static bool DeleteMode = false;
-        private static String MatchText = string.Empty;
-        private static List<Dictionary<string, List<string>>> oemFileInfoSelectedList = new List<Dictionary<string, List<string>>>();
+        private static bool _deleteMode;
+        private static string _matchText = string.Empty;
+
+        private static readonly List<Dictionary<string, List<string>>> OemFileInfoSelectedList =
+            new List<Dictionary<string, List<string>>>();
 
         private static void Main(string[] args)
         {
             StreamWriter sw = null;
-            foreach (string arg in args)
-            {
+            foreach (var arg in args)
                 if (arg.ToLower().StartsWith("/out:"))
                 {
                     sw = File.CreateText(arg.Substring(5));
@@ -23,10 +23,10 @@ namespace PnpFind
                 }
                 else if (arg.ToLower() == "/delete")
                 {
-                    DeleteMode = true;
+                    _deleteMode = true;
                 }
-                else if (arg.ToLower() == "/?" || 
-                    (arg.ToLower() == "--help"))
+                else if (arg.ToLower() == "/?" ||
+                         arg.ToLower() == "--help")
                 {
                     Console.WriteLine("PnpFind - Driverstore Destroyer");
                     Console.WriteLine("Copyright (C) 2011 Travis Robinson");
@@ -36,68 +36,61 @@ namespace PnpFind
                 }
                 else
                 {
-                    MatchText = arg;
+                    _matchText = arg;
                 }
-            }
 
-            List<FileInfo> oemFileList = DriverStore.GetOemInfFileList();
-            foreach (FileInfo oemFileInfo in oemFileList)
+            var oemFileList = DriverStore.GetOemInfFileList();
+            foreach (var oemFileInfo in oemFileList)
             {
-                bool matched = MatchText == String.Empty ? true : oemFileInfo.Name.ToLower().Contains(MatchText);
+                var matched = _matchText == string.Empty || oemFileInfo.Name.ToLower().Contains(_matchText);
 
-                Dictionary<string, List<string>> infEntities;
-                DriverStore.GetInfSection(oemFileInfo.FullName, "Version", out infEntities);
-                infEntities.Add("Inf", new List<string>(new String[] { oemFileInfo.Name}));
+                DriverStore.GetInfSection(oemFileInfo.FullName, "Version", out var infEntities);
+                infEntities.Add("Inf", new List<string>(new[] {oemFileInfo.Name}));
                 infEntities.Remove("Signature");
                 infEntities.Remove("signature");
-                foreach (KeyValuePair<string, List<string>> infEntity in infEntities)
+                foreach (var infEntity in infEntities)
                 {
-                    foreach (string  value in infEntity.Value)
+                    foreach (var value in infEntity.Value)
                     {
-                        if (MatchText == String.Empty)
+                        if (_matchText == string.Empty)
                             matched = true;
-                        else if (value.ToLower().Contains(MatchText.ToLower()))
+                        else if (value.ToLower().Contains(_matchText.ToLower()))
                             matched = true;
 
                         if (matched) break;
-                     
                     }
+
                     if (matched) break;
                 }
-                if (matched)
-                {
-                    oemFileInfoSelectedList.Add(infEntities);
-                }
+
+                if (matched) OemFileInfoSelectedList.Add(infEntities);
             }
 
-            foreach (Dictionary<string, List<string>> infEntities in oemFileInfoSelectedList)
+            foreach (var infEntities in OemFileInfoSelectedList)
             {
-                string infFile = String.Empty;
-                foreach (KeyValuePair<string, List<string>> infEntity in infEntities)
+                var infFile = string.Empty;
+                foreach (var infEntity in infEntities)
                 {
-                    String valueDisplayText = String.Empty;
-                    foreach (string infValue in infEntity.Value)
+                    var valueDisplayText = string.Empty;
+                    foreach (var infValue in infEntity.Value)
                         valueDisplayText += infValue + ", ";
 
-                    if (valueDisplayText.Length >=2)
+                    if (valueDisplayText.Length >= 2)
                         valueDisplayText = valueDisplayText.Substring(0, valueDisplayText.Length - 2);
 
                     Console.WriteLine("{0,-15} : {1}", infEntity.Key, valueDisplayText);
 
-                    if (infEntity.Key.ToLower()=="inf")
-                    {
-                        infFile = valueDisplayText;
-                    }
+                    if (infEntity.Key.ToLower() == "inf") infFile = valueDisplayText;
                 }
+
                 Console.WriteLine();
 
-                if (DeleteMode && infFile != String.Empty)
+                if (_deleteMode && infFile != string.Empty)
                 {
-                    if (MatchText==string.Empty)
-                    {
-                        throw new Exception("aborting because empty match string would result in total driverstore annihilation.");
-                    }
-                    int result = DriverStore.RemoveOemInf(infFile);
+                    if (_matchText == string.Empty)
+                        throw new Exception(
+                            "aborting because empty match string would result in total driverstore annihilation.");
+                    var result = DriverStore.RemoveOemInf(infFile);
                     switch (result)
                     {
                         case 0:
@@ -111,6 +104,7 @@ namespace PnpFind
                             catch (Exception)
                             {
                             }
+
                             break;
                         default:
                             Console.WriteLine("Failed deleting {0} result={1}", infFile, result);
@@ -119,7 +113,7 @@ namespace PnpFind
                 }
             }
 
-            if (!ReferenceEquals(null,sw))
+            if (!ReferenceEquals(null, sw))
             {
                 sw.Flush();
                 sw.Close();
